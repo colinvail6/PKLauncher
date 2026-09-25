ad282d8b30b97bfb1d7f6c8fbafb821b1b8f1da0
Colin <colinvail6@gmail.com>
2026-09-24 18:52:37 -0700

    Initialize FastFire module with configuration function
    Added a module header and imports for fire effects.

--- kpk/FastFire.py
+++ kpk/FastFire.py
@@ -1,1 +1,14 @@
+# FastFire.py - a module based off of MatrixFireFast to create fire effects for the launcher
+import pixelkit as kit
+import time, random
 
+# Defaults
+width = 16
+height = 8
+
+def configure(w, h, framerate, colors): # example: fire.configure(16, 8, [list of colors])
+  global width
+  global height
+  width = w
+  height = h
+  ncolors = (len(colors)/len(colors[0])
